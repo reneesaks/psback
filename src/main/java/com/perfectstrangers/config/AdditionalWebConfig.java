d3f49ccbbe933645b510e88c9a428a655c036f56
reneesaks <renee.saks@khk.ee>
2018-05-09 20:54:33 +0300

    Refactoring CORS configuration

--- src/main/java/com/perfectstrangers/config/AdditionalWebConfig.java
+++ src/main/java/com/perfectstrangers/config/AdditionalWebConfig.java
@@ -1,6 +1,7 @@
 package com.perfectstrangers.config;
 
 import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,20 @@ import org.springframework.web.filter.CorsFilter;
 @Profile({"staging", "production"})
 public class AdditionalWebConfig {
 
+    private static List<String> allowedOrigins = Arrays.asList(
+            "https://199.247.13.206",   // Front end HTTPS
+            "https://144.202.77.179",   // Back end HTTPS
+            "http://localhost:4200"     // Localhost HTTP
+    );
+
+    private static List<String> allowedHeaders = Arrays.asList(
+            "Authorization", "Content-Type"
+    );
+
+    private static List<String> allowedMethods = Arrays.asList(
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+    );
+
     /**
      * CORS configuration.
      *
@@ -26,14 +41,14 @@ public class AdditionalWebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://199.247.13.206");
-        config.addAllowedOrigin("https://144.202.77.179 ");
-        config.addAllowedOrigin("https://localhost:4200 ");
-        config.addAllowedOrigin("127.0.0.1");
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(allowedHeaders);
+        config.setAllowedMethods(allowedMethods);
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        CorsFilter corsFilter = new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
+
         bean.setOrder(0);
         return bean;
     }
